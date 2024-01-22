59abf168268a7d521ac68a770ef609af323f4e7f
alexsar kodehode <alexsar.kodehode@gmail.com>
2024-01-22 19:53:35 +0100

    Made the counter component

---
+++ reactive variables/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import CounterButtons from './CounterButtons'
+
+
+
+function App() {
+  
+  return (
+    <>
+     <div>
+      <h1>reactLive Buttons</h1>
+      <h3>Conditional Buttons</h3>
+      <CounterButtons></CounterButtons>
+     </div>
+    </>
+  )
+}
+
+export default App
