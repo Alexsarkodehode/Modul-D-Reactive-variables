fca3b969eba2d3252ba9e0e55ebbd887ded3e44c
alexsar kodehode <alexsar.kodehode@gmail.com>
2024-01-22 22:21:33 +0100

    ok

--- reactive variables/src/CounterButtons.jsx
+++ reactive variables/src/CounterButtons.jsx
@@ -1,7 +1,10 @@
+//usestate gjør sånn at vi kan bruke state i komponentene våre 
 import {useState} from 'react'
 
+//funksjon for CounterButtons
 function CounterButtons(){
     const [count, setCount] = useState(0);
+    //For å bruke en counter så trenger vi tre knapper. Økende, senkende og restartende. 
     return (
         <>
           <button className="increment" onClick={() => setCount(count+1)}>Increment</button>
